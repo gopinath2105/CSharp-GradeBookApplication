ca3872c64b0c2e7fda3702c29d3a181172098ff7
gopinath2105 <gopinath.chandhikaa@gmail.com>
2022-02-12 18:14:50 +0530

    Updated Module 1 changes

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,6 +8,8 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
+            Name = name;
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }

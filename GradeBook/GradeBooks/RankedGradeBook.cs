6db7af97650a0c13c62399aef8880a2fb3a4e1e8
gopinath2105 <gopinath.chandhikaa@gmail.com>
2022-02-14 07:03:49 +0530

    Module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace GradeBook.GradeBooks
 {
@@ -55,4 +53,4 @@ namespace GradeBook.GradeBooks
             base.CalculateStudentStatistics(name);
         }
     }
-}
+}   
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GradeBook.GradeBooks
+﻿namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Name = name;
             Type = Enums.GradeBookType.Standard;
         }
     }
